0470eda720466e029a465aa60e2d56c5894c9918
namhkoh <43123189+namhkoh@users.noreply.github.com>
2020-10-01 23:29:05 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,25 +1,27 @@
-var express = require('express'); 
-var app = express(); 
- 
-const bodyParser = require('body-parser'); 
-// var server = app.listen(3000); 
- 
-app.use(bodyParser.json()); 
-app.use(bodyParser.urlencoded({ extended: true })); 
- 
-app.post('/postdata', (req, res) => { 
-    var data = req.body.data; // your data 
-    console.log(data)
-    // do something with that data (write to a DB, for instance) 
-	res.status(200).json({ 
-		message: "Data received successfully" 
-	}); 
-}); 
+var express = require("express");
+var app = express();
+
+const bodyParser = require("body-parser");
+// var server = app.listen(3000);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/postdata", (req, res) => {
+  var data = req.body.data; // your data
+  console.log(data);
+  // do something with that data (write to a DB, for instance)
+  res.status(200).json({
+    message: "Data received successfully",
+  });
+});
 
 // app.get('/postdata',function(req,res){
 //     res.send('Appliance Helper test');
 // });
 
-app.listen(3001, function (req,res){
-    console.log('Running...');
+app.listen(3001, function (req, res) {
+  console.log("Running...");
 });
+
+
