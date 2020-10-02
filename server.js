2ab9d569140e0d9a13d339cbdafbaae2f895c7d6
namhkoh <43123189+namhkoh@users.noreply.github.com>
2020-10-02 16:49:05 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,32 +1,10 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
-const bodyParser = require("body-parser");
-// var server = app.listen(3000);
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.post("/postdata", (req, res) => {
-  var data = req.body.data; // your data
-  console.log(data);
-  // do something with that data (write to a DB, for instance)
-  res.status(200).json({
-    message: "Data received successfully",
-  });
+app.use(myParser.urlencoded({ extended: true }));
+app.post("/sendData", function (request, response) {
+  console.log(request.User); 
 });
 
-app.get("/postdata", function (req, res) {
-  // res.send('Appliance Helper test');
-  var data = req.body.data; // your data
-  req.addListener("data", function (chunk) {
-    data += chunk;
-  });
-  req.addListener("end", function () {
-    console.log("from android :" + data);
-  });
-});
-
-app.listen(3030, function (req, res) {
-  console.log("Running...");
-});
+app.listen(3030);
