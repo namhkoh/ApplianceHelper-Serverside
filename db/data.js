08757b55605c74b3fab12d481a8a489cdc85d4b4
namhkoh <43123189+namhkoh@users.noreply.github.com>
2020-10-02 23:47:02 +0100

    Update data.js
    removed sample data for testing.

--- db/data.js
+++ db/data.js
@@ -1,39 +1,39 @@
 module.exports = [
-    {
-      testId: 1,
-      name: "John Doe",
-      buttonsCorrect: 10,
-      buttonsIncorrect: 2,
-      startSession: 1243242,
-      endSession: 4234123,
-      totalTime: 1343432,
-      userConsent: true,
-      feedback: {
-          uiVariant1: 3,
-          uiVariant2: 5,
-          uiVariant3: 5,
-          uiVariant4: 3,
-          uiVariant5: 4,
-          uiVariant6: 6,
-      }
-    },
-    {
-        testId: 2,
-        name: "Charles Opper",
-        buttonsCorrect: 10,
-        buttonsIncorrect: 2,
-        startSession: 1243242,
-        endSession: 4234123,
-        totalTime: 1343432,
-        userConsent: true,
-        feedback: {
-            uiVariant1: 3,
-            uiVariant2: 5,
-            uiVariant3: 5,
-            uiVariant4: 3,
-            uiVariant5: 4,
-            uiVariant6: 6,
-        }
-      }
+    // {
+    //   testId: 1,
+    //   name: "John Doe",
+    //   buttonsCorrect: 10,
+    //   buttonsIncorrect: 2,
+    //   startSession: 1243242,
+    //   endSession: 4234123,
+    //   totalTime: 1343432,
+    //   userConsent: true,
+    //   feedback: {
+    //       uiVariant1: 3,
+    //       uiVariant2: 5,
+    //       uiVariant3: 5,
+    //       uiVariant4: 3,
+    //       uiVariant5: 4,
+    //       uiVariant6: 6,
+    //   }
+    // },
+    // {
+    //     testId: 2,
+    //     name: "Charles Opper",
+    //     buttonsCorrect: 10,
+    //     buttonsIncorrect: 2,
+    //     startSession: 1243242,
+    //     endSession: 4234123,
+    //     totalTime: 1343432,
+    //     userConsent: true,
+    //     feedback: {
+    //         uiVariant1: 3,
+    //         uiVariant2: 5,
+    //         uiVariant3: 5,
+    //         uiVariant4: 3,
+    //         uiVariant5: 4,
+    //         uiVariant6: 6,
+    //     }
+    // }
 ]
 
