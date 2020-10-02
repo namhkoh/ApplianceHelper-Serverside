5cd24d7f00e3382eae08b473a0672429c069e966
namhkoh <43123189+namhkoh@users.noreply.github.com>
2020-10-02 16:55:12 +0100

    Update server.js

--- server.js
+++ server.js
@@ -7,4 +7,14 @@ app.post("/sendData", function (request, response) {
   console.log(request.User); 
 });
 
-app.listen(3030);
+app.get("/getData", function (req, res) {
+  // res.send('Appliance Helper test');
+  var data = req.User; // your data
+  req.addListener("User", function () {
+    console.log("from android :" + data);
+  });
+});
+
+app.listen(3030, function (req, res) {
+  console.log("Running...");
+});
