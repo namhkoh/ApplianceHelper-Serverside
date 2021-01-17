d88c27b512914dc4f719b1c934c1ae30c11fd329
namhkoh <43123189+namhkoh@users.noreply.github.com>
2021-01-17 17:46:48 +0900

    Added new data fields.

--- server.js
+++ server.js
@@ -1,71 +1,67 @@
 var express = require("express");
-var db = require("../ApplianceHelper-Serverside/db/data");
+// var db = require("../ApplianceHelper-Serverside/db/data");
+var db = require('./db/data');
 var bodyParser = require("body-parser");
-const fs = require('fs')
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
+const { json } = require("body-parser");
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-const dirPath = path.join(__dirname, '/storedData');
-const dir = './storedData'
+const dirPath = path.join(__dirname, "/storedData");
+const dir = "./storedData";
 if (fs.existsSync(dir)) {
   console.log("Directory exits");
 } else {
-  fs.mkdirSync(dirPath); 
+  fs.mkdirSync(dirPath);
 }
 
 app.get("/api/v1/getData", (req, res) => {
-  let rawDataOutput = fs.readFileSync('storedData/output.json');
-  let parsed = JSON.parse(rawDataOutput)
+  // let rawDataOutput = fs.readFileSync('storedData/output.json');
+  // let parsed = JSON.parse(rawDataOutput)
   res.status(200).send({
     success: "true",
     message: "userData retrieved successfully",
-    userData: parsed,
+    userData: db,
   });
 });
 const PORT = 3030;
 
 app.post("/api/v1/todos", (req, res) => {
-  try{
+  try {
     if (!req.body.testId) {
       return res.status(400).send({
         success: "false",
         message: "invalid testId",
       });
-    } 
-    else if (!req.body.name) {
+    } else if (!req.body.name) {
       return res.status(400).send({
         success: "false",
         message: "invalid name",
       });
-    } 
-    else if (!req.body.startSession) {
+    } else if (!req.body.startSession) {
       return res.status(400).send({
         success: "false",
         message: "invalid epoch time",
       });
-    } 
-    else if (!req.body.endSession) {
+    } else if (!req.body.endSession) {
       return res.status(400).send({
         success: "false",
         message: "invalid epoch time",
       });
-    } 
-    else if (!req.body.totalTime) {
+    } else if (!req.body.totalTime) {
       return res.status(400).send({
         success: "false",
         message: "invalid epoch time",
       });
-    } 
-    else if (!req.body.userConsent) {
+    } else if (!req.body.userConsent) {
       return res.status(400).send({
         success: "false",
         message: "invalid boolean",
       });
-    } 
-    else if (!req.body.feedback) {
+    } else if (!req.body.feedback) {
       return res.status(400).send({
         success: "false",
         message: "invalid score value",
@@ -76,33 +72,34 @@ app.post("/api/v1/todos", (req, res) => {
         message: "invalid questions",
       });
     }
-  } catch(e){
+    const userData = {
+      testId: req.body.testId,
+      name: req.body.name,
+      startSession: req.body.startSession,
+      endSession: req.body.endSession,
+      totalTime: req.body.totalTime,
+      userConsent: req.body.userConsent,
+      feedback: req.body.feedback,
+      userSequence: req.body.userSequence,
+    };
+    console.log(userData);
+    db.push(userData);
+    fs.appendFile(
+      "storedData/output.json",
+      JSON.stringify(userData),
+      function (err) {
+        if (err) throw err;
+        console.log("Saved!");
+      }
+    );
+    return res.status(201).send({
+      success: "true",
+      message: "users added successfully",
+      userData: userData,
+    });
+  } catch (e) {
     console.log("ERROR " + e);
   }
-  
-  const userData = {
-    testId: req.body.testId,
-    name: req.body.name,
-    startSession: req.body.startSession,
-    endSession: req.body.endSession,
-    totalTime: req.body.totalTime,
-    userConsent: req.body.userConsent,
-    feedback: req.body.feedback,
-    questions: req.body.userSequence
-  };
-  console.log(userData);
-
-  fs.appendFile("storedData/output.json", JSON.stringify(userData), function (err) {
-    if (err) throw err;
-      console.log('Saved!');
-  });
-
-  db.push(userData);
-  return res.status(201).send({
-    success: "true",
-    message: "users added successfully",
-    userData,
-  });
 });
 
 app.get("/api/v1/todos/:testId", (req, res) => {
@@ -122,24 +119,24 @@ app.get("/api/v1/todos/:testId", (req, res) => {
   });
 });
 
-app.delete('/api/v1/todos/:testId', (req, res) => {
+app.delete("/api/v1/todos/:testId", (req, res) => {
   const testId = parseInt(req.params.testId, 10);
-  db.map((userData,index) => {
+  db.map((userData, index) => {
     if (userData.testId === testId) {
-       db.splice(index, 1);
-       return res.status(200).send({
-         success: 'true',
-         message: 'testId deleted successfully',
-       });
+      db.splice(index, 1);
+      return res.status(200).send({
+        success: "true",
+        message: "testId deleted successfully",
+      });
     }
   });
-    return res.status(404).send({
-      success: 'false',
-      message: 'testId not found',
-    });
-
+  return res.status(404).send({
+    success: "false",
+    message: "testId not found",
+  });
 });
 
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
 });
+
